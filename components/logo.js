6f2006332ba34ccda2abca73f4633e6353d96ebc
Jerry Wan <craftersol728@gmail.com>
2023-04-08 09:15:07 -0700

    added logo.js folder

---
+++ components/logo.js
@@ -0,0 +1,6 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { text , useColorModeValue } from '@chakra-ui/react'
+
+import styled from '@emotion/styled'
+const LogoBox = styled.span'
