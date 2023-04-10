e1445dbddf15f10dc5a100913517058f87d94ac2
Jerry Wan <craftersol728@gmail.com>
2023-04-09 20:46:52 -0700

    added section.js for work

---
+++ components/fonts.js
@@ -0,0 +1,11 @@
+import {Global} from '@emotion/react'
+
+const Fonts = () => (
+<Global 
+    styles = {`
+    @import url('@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,500;1,500;1,800&family=Ubuntu:wght@500&display=swap');
+    `}
+    /> 
+  )
+  export default Fonts;
+  
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { Container , Box, Heading } from "@chakra-ui/react"
-
+import { Container , Box, Heading ,Image, useColorModeValue} from "@chakra-ui/react"
+import Section from "../components/section"
 const Page = () => {
   return (
     <Container>
-        <Box borderRadius="lg" bg="red" mb={6} p={3} align="center">
+        <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} mb={6} p={3} align="center">
             Hello, I&apos;m a full-stack developer. 
         </Box>
 
@@ -14,8 +14,17 @@ const Page = () => {
                 </Heading>
                 <p>Just me ( Developer / Gamer / Toatally Amazing Person)</p>
             </Box>
-        </Box>
+            <Box flexShrink={0} mt={{base: 4, md:0}} ml={{md:6}} align="center">
+                <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth ="100px" display="inline-block" borderRadius="full" src="/images/unknown.png" alt="Profile Image"/>
 
+            </Box>
+        </Box>
+        <Section delay={0.1}>
+            <Heading as="h3" variant="section-title">
+                Work
+            </Heading>
+            <p>Here are some of my works.</p>
+        </Section>
     </Container>
   )
 }
