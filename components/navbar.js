fc5894a343bdcc3a83eb25f405b86941bd81a704
Jerry Wan <craftersol728@gmail.com>
2023-04-09 22:12:17 -0700

    fixed error with robbert and added resume link

--- components/navbar.js
+++ components/navbar.js
@@ -1,117 +1,138 @@
-import Logo from './logo';
-import NextLink from 'next/link';
-
+import { forwardRef } from 'react'
+import Logo from './logo'
+import NextLink from 'next/link'
 import {
-    Container,
-    Box,
-    Link,
-    Stack,
-    Heading,
-    Flex,
-    Menu,
-    MenuItem,
-    MenuList,
-    MenuButton,
-    IconButton,
-    useColorModeValue,
-} from '@chakra-ui/react';
-
-import { HamburgerIcon } from '@chakra-ui/icons';
-import ThemeToggleButton from './theme-toggle-button';
-
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, target, children, ...props }) => {
-    const active = path === href
-    const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
-    return (
-      <Link
-        as={NextLink}
-        href={href}
-        scroll={false}
-        p={2}
-        bg={active ? 'grassTeal' : undefined}
-        color={active ? '#202023' : inactiveColor}
-        target={target}
-        {...props}
-      >
-        {children}
-      </Link>
-    )
-  }
-
-const Navbar = props => {
-    const {path} = props;
-
-    return(
-        <Box 
-        position="fixed"
-        as="nav"
-        w="100%"
-        bg={useColorModeValue('$ffffff40','#20202380')}
-        style={{backdropFilter: 'blur(10px)'}}
-        zIndex={1}
-        {...props}
-        >   
-        <Container 
-        display="flex" 
-        p={2} 
-        maxW="container.md" 
-        wrap="wrap" 
-        align="center" 
-        justify="space-between"
-        >
-            <Flex align="center" mr={5}>
-                <Heading as="h1" size="lg" letterSpacing={'tighter'}>
-                    <Logo />
-                </Heading>
-            </Flex>
-
-            <Stack
-                direction={{ base: 'column', md: 'row' }}
-                display={{ base: 'none', md: 'flex' }}
-                width={{ base: 'full', md: 'auto' }}
-                alignItems="center"
-                flexGrow={1}
-                mt={{ base: 4, md: 0 }}
-                >
-                    <LinkItem href="/works" path={path}>Works</LinkItem>
-                    <LinkItem href="/posts" path={path}>Posts</LinkItem>
-            </Stack>
-
-            <Box flex={1} align="right">
-                <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
-                    <Menu>
-                    <ThemeToggleButton />
-                        <MenuButton as={IconButton} icon={<HamburgerIcon />} variant="outline "aria-label="Options" />
-                        <MenuList>
-                        <NextLink href="/" passHref>
-                            <MenuItem as = {Link}>About</MenuItem>
-                        </NextLink>                            
-                        <NextLink href="/works" passHref>
-                            <MenuItem as = {Link}>Works</MenuItem>
-                        </NextLink>
-                        <NextLink href="/posts" passHref>
-                            <MenuItem as = {Link}>Posts</MenuItem>
-                         </NextLink>
-                         <NextLink href="/https://github.com/craftersol728/Jerry-s-Personal-Page" passHref>
-                            <MenuItem as = {Link}>View Scource</MenuItem>
-                         </NextLink>
-
-                         <NextLink href="/https://docs.google.com/document/d/e/2PACX-1vRy4zFlF6kdrh0Rq2LSzg1q7c8XEzbyfByZJSpGZsR_CtG0W4ZXNpTUWM0TM-0OqCk3su1tUgtT2z4P/pub" passHref>
-                            <MenuItem as = {Link}>Resume</MenuItem>
-                         </NextLink>
-
-                         <NextLink href="/https://github.com/craftersol728/Jerry-s-Personal-Page" passHref>
-                            <MenuItem as = {Link}>View Scource</MenuItem>
-                         </NextLink>
-                        </MenuList>
-                        
-                    </Menu>
-                </Box> 
-            </Box> 
-        </Container>
-        </Box>
-    )
+  const active = path === href
+  const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
+  return (
+    <Link
+      as={NextLink}
+      href={href}
+      scroll={false}
+      p={2}
+      bg={active ? 'grassTeal' : undefined}
+      color={active ? '#202023' : inactiveColor}
+      target={target}
+      {...props}
+    >
+      {children}
+    </Link>
+  )
 }
 
-export default Navbar;
+const MenuLink = forwardRef((props, ref) => (
+  <Link ref={ref} as={NextLink} {...props} />
+))
+
+const Navbar = props => {
+  const { path } = props
+
+  return (
+    <Box
+      position="fixed"
+      as="nav"
+      w="100%"
+      bg={useColorModeValue('#ffffff40', '#20202380')}
+      css={{ backdropFilter: 'blur(10px)' }}
+      zIndex={2}
+      {...props}
+    >
+      <Container
+        display="flex"
+        p={2}
+        maxW="container.md"
+        wrap="wrap"
+        align="center"
+        justify="space-between"
+      >
+        <Flex align="center" mr={5}>
+          <Heading as="h1" size="lg" letterSpacing={'tighter'}>
+            <Logo />
+          </Heading>
+        </Flex>
+
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          display={{ base: 'none', md: 'flex' }}
+          width={{ base: 'full', md: 'auto' }}
+          alignItems="center"
+          flexGrow={1}
+          mt={{ base: 4, md: 0 }}
+        >
+          <LinkItem href="/works" path={path}>
+            Works
+          </LinkItem>
+          <LinkItem href="/posts" path={path}>
+            Posts
+          </LinkItem>
+          <LinkItem href="https://docs.google.com/document/d/e/2PACX-1vRy4zFlF6kdrh0Rq2LSzg1q7c8XEzbyfByZJSpGZsR_CtG0W4ZXNpTUWM0TM-0OqCk3su1tUgtT2z4P/pub">Resume</LinkItem>
+          <LinkItem
+            target="_blank"
+            href="https://github.com/craftersol728"
+            path={path}
+            display="inline-flex"
+            alignItems="center"
+            style={{ gap: 4 }}
+            pl={2}
+          >
+            Source
+          </LinkItem>
+        </Stack>
+
+        <Box flex={1} align="right">
+          <ThemeToggleButton />
+
+          <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+            <Menu isLazy id="navbar-menu">
+              <MenuButton
+                as={IconButton}
+                icon={<HamburgerIcon />}
+                variant="outline"
+                aria-label="Options"
+              />
+              <MenuList>
+                <MenuItem as={MenuLink} href="/">
+                  About
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/works">
+                  Works
+                </MenuItem>
+                <MenuItem as={MenuLink} href="/posts">
+                  Posts
+                </MenuItem>
+                <MenuItem as={MenuLink} href="https://docs.google.com/document/d/e/2PACX-1vRy4zFlF6kdrh0Rq2LSzg1q7c8XEzbyfByZJSpGZsR_CtG0W4ZXNpTUWM0TM-0OqCk3su1tUgtT2z4P/pub">
+                  Resume
+                </MenuItem>
+                <MenuItem
+                  as={Link}
+                  href="https://github.com/craftersol728"
+                >
+                  View Source
+                </MenuItem>
+              </MenuList>
+            </Menu>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  )
+}
+
+export default Navbar
