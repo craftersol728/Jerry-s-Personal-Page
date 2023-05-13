ed8950025a3d8e437252f4aea2f3e210aead91de
Jerry Wan <craftersol728@gmail.com>
2023-05-12 21:44:34 -0700

    added side projects

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const Page = () => {
     <Layout>
     <Container>
         <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500','whiteAlpha.200')} mb={6} p={3} align="center">
-            Hello, I&apos;m a full-stack developer. 
+            Hello, I&apos;m  full-stack developer. 
         </Box>
 
         <Box display={{md: "flex"}}>
@@ -29,7 +29,7 @@ const Page = () => {
                 <Heading as="h2" variant="page-title">
                     Jerry Wan
                 </Heading>
-                <p>Just me ( Developer / Gamer / Toatally Amazing Person)</p>
+                <p>Just me ( Developer / Fast Learner / Quick Thinker / Problem Solver)</p>
             </Box>
             <Box flexShrink={0} mt={{base: 4, md:0}} ml={{md:6}} align="center">
                 <Image borderColor="whiteAlpha.800" borderWidth={2} borderStyle="solid" maxWidth ="100px" display="inline-block" borderRadius="full" src="/images/unknown.png" alt="Profile Image"/>
@@ -78,6 +78,69 @@ const Page = () => {
                 <BioYear>2023</BioYear>
                 Completed the UC Berkeley Full Stack Coding Bootcamp and became a full-stack developer 
             </BioSection>
+        </Section>
+        <Section delay={0.2}>
+            <Heading as="h3" variant="section-title">
+                Side Projects
+            </Heading>
+            <BioSection>
+                <BioYear>Taxplus</BioYear><br />
+                Currently rebuilding the entire Taxplus website. Using React HTML Typescript and CSS to create a responsive website.
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Professional Portfolio</BioYear>
+                <br />
+                Used HTML and CSS to create a responsive portfolio website with animations.
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Password Generator</BioYear>
+                <br />
+                Built a dynamic web application that an employee can use to generate a random password based on criteria they've selected. powered by Javascript code!
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Web APIs：Code Quiz</BioYear>
+                <br />
+                Built a timed coding quiz with multiple-choice questions. This app will run in the browser, and will feature dynamically updated HTML and CSS powered by JavaScript code. It has a clean, polished, and responsive user interface. 
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Third-Party APIs : Work Day Scheduler</BioYear>
+                <br />
+                Created a simple calendar application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Node.js: Professional README Generator</BioYear>
+                <br />
+                Created a Professional README Generator application that generates a READ ME.md file that includes what the app is for, how to use the app, how to install it, how to report issues, and how to make contributions.
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Object-Oriented Programming : Team Profile Generator</BioYear>
+                <br />
+                Created a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Express.js : Note Taker</BioYear>
+                <br />
+                Created an application that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+            </BioSection>
+            <br />
+            <BioSection>
+                <BioYear>Object-Oriented Programming : Team Profile Generator</BioYear>
+                <br />
+                Created a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+            </BioSection>
+            <br />
+
+
+
+
+
         </Section>
 
         <Section delay={0.3}>
