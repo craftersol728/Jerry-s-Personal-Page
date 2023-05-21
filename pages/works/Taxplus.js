8c4992e253a36781ad100df99ffde7b5f7f06e4a
Jerry Wan <craftersol728@gmail.com>
2023-05-21 15:55:49 -0700

    removed an image

--- pages/works/Taxplus.js
+++ pages/works/Taxplus.js
@@ -65,7 +65,6 @@ import {
           <Center>More Details</Center>
         </Heading>
         <SimpleGrid columns={2} gap={2}>
-        <WorkImage src="/images/works/taxplus.png" alt="BBB" />
         <WorkImage src="/images/works/taxplus1.png" alt="BBB" />
         <WorkImage src="/images/works/taxplus2.png" alt="BBB" />
         <WorkImage src="/images/works/taxplus3.png" alt="BBB" />
